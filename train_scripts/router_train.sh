c6d2f01f435921650b9dcd89baee808da18f2e81
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2026-05-12 11:39:59 -0600

    change the batch_size

--- train_scripts/router_train.sh
+++ train_scripts/router_train.sh
@@ -11,7 +11,7 @@ cluster_loss_weight=1
 seeds=(1)
 
 for seed in "${seeds[@]}"; do
-    ./venv/bin/python3 -u train_router_mdeberta.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --final_eval --seed ${seed} --batch_size 16 \
+    ./venv/bin/python3 -u train_router_mdeberta.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --final_eval --seed ${seed} --batch_size 64 \
     --data_paths "./datasets/split2_model7_cluster/gsm8k-train.json" "./datasets/split2_model7_cluster/humaneval_train.json"  "./datasets/split2_model7_cluster/arc_challenge_train.json" "./datasets/split2_model7_cluster/cmmlu_train.json" "./datasets/split2_model7_cluster/mmlu_train.json" \
     --test_data_paths "./datasets/split2_model7/gsm8k-test.json" "./datasets/split2_model7/humaneval_test.json" "./datasets/split2_model7/arc_challenge_test.json" "./datasets/split2_model7/cmmlu_test.json" "./datasets/split2_model7/mmlu_test.json" \
     --save_path "./logs/RouterDC/run1" \
