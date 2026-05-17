4253010c8187574ac3d2c3b05bc2ecb5715b4dc7
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-17 14:54:57 -0600

    add data preprocessing code for primevul

---
+++ eval_scripts_security_tasks/test.sh
@@ -0,0 +1,11 @@
+model_path="codellama/CodeLlama-7b-Instruct-hf"
+model_name="CodeLlama-7b-Instruct"
+
+# primevul
+lm_eval --model vllm \
+    --model_args "pretrained=$model_path,max_model_len=26653" \
+    --tasks primevul \
+    --batch_size auto \
+    --output_path "./output/primevul/${model_name}" \
+    --log_samples \
+    --include_path ./custom_tasks_lm_eval
