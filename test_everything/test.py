e5a577e0142e91fba3a059a346f3afb873aaabfd
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-17 23:44:52 -0600

    use train dataset

--- test_everything/test.py
+++ test_everything/test.py
@@ -1,11 +1,3 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer
-
 ds = load_dataset("colin/PrimeVul", "paired")
-tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-Instruct-hf")
-
-lengths = [len(tokenizer(x["func"])["input_ids"]) for x in ds["test"]]
-print(f"Max: {max(lengths)}, Mean: {sum(lengths)/len(lengths):.0f}")
-
-over_8192 = sum(1 for l in lengths if l > 26653)
-print(f"{over_8192}/{len(lengths)} = {over_8192/len(lengths)*100:.1f}%")
+print(ds)
