e4ca2ae8738f34ac6b89c1a27225bc599ce49397
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-19 11:00:02 -0600

    reduce max length token

--- eval_scripts_security_tasks/eval_primevul_gen.sh
+++ eval_scripts_security_tasks/eval_primevul_gen.sh
@@ -2,7 +2,7 @@ model_path="codellama/CodeLlama-7b-Instruct-hf"
 model_name="CodeLlama-7b-Instruct"
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192" \
+    --model_args "pretrained=$model_path,max_model_len=4096" \
     --tasks primevul_gen \
     --batch_size auto \
     --output_path "./output/primevul_gen/${model_name}" \
