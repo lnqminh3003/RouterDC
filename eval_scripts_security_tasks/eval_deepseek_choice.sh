272773346d498b0dcc13d43106adb6d6d867be34
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-22 16:09:48 -0600

    fix deepseek

--- eval_scripts_security_tasks/eval_deepseek_choice.sh
+++ eval_scripts_security_tasks/eval_deepseek_choice.sh
@@ -4,9 +4,9 @@ model_name="DeepSeek-Coder-V2-Lite-Instruct"
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.80" \
     --tasks primevul_choice_1to2 \
-    --batch_size auto \
+    --batch_size 16 \
     --output_path "./output/primevul_choice_1to2/${model_name}" \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
