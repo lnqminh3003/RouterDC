2fbb91348cc0b71171c47bbdd725f7513b5f63e1
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-22 15:28:59 -0600

    fix error for codellama 13B

--- eval_scripts_security_tasks/eval_codellama_gen.sh
+++ eval_scripts_security_tasks/eval_codellama_gen.sh
@@ -2,7 +2,7 @@ model_path="codellama/CodeLlama-13b-Instruct-hf"
 model_name="CodeLlama-13b-Instruct"
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=4096" \
+    --model_args "pretrained=$model_path,max_model_len=8192,gpu_memory_utilization=0.95" \
     --tasks primevul_gen_1to2 \
     --batch_size auto \
     --output_path "./output/primevul_gen_1to2/${model_name}" \
