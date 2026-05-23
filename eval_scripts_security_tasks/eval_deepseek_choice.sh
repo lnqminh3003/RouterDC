2bc80fa53540df0b60af34101565701de80863eb
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-23 10:18:20 -0600

    fix issue run out of memory when evaluating LLMs

--- eval_scripts_security_tasks/eval_deepseek_choice.sh
+++ eval_scripts_security_tasks/eval_deepseek_choice.sh
@@ -2,9 +2,10 @@ model_path="deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
 model_name="DeepSeek-Coder-V2-Lite-Instruct"
 
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=4096,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.90" \
+    --model_args "pretrained=$model_path,max_model_len=4096,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.65,enforce_eager=True,max_num_batched_tokens=4096" \
     --tasks primevul_choice_1to2 \
     --batch_size 8 \
     --output_path "./output/primevul_choice_1to2/${model_name}" \
--- eval_scripts_security_tasks/eval_virtue_choice.sh
+++ eval_scripts_security_tasks/eval_virtue_choice.sh
@@ -2,11 +2,12 @@ model_path="Virtue-AI-HUB/VulnLLM-R-7B"
 model_name="VulnLLM-R-7B"
 
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.90" \
+    --model_args "pretrained=$model_path,max_model_len=4096,trust_remote_code=True,gpu_memory_utilization=0.65,enforce_eager=True,max_num_batched_tokens=4096" \
     --tasks primevul_choice_1to2 \
-    --batch_size auto \
+    --batch_size 8 \
     --output_path "./output/primevul_choice_1to2/${model_name}" \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
