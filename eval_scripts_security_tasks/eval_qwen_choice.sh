6c7adf1767a883fd947cfa6c4a09331ebd87daf4
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-23 00:02:21 -0600

    fix issue running for qwen

--- eval_scripts_security_tasks/eval_qwen_choice.sh
+++ eval_scripts_security_tasks/eval_qwen_choice.sh
@@ -4,7 +4,7 @@ model_name="Qwen2.5-Coder-14B-Instruct"
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.85" \
+    --model_args "pretrained=$model_path,max_model_len=4096,trust_remote_code=True,gpu_memory_utilization=0.90,tensor_parallel_size=2" \
     --tasks primevul_choice_1to2 \
     --batch_size auto \
     --output_path "./output/primevul_choice_1to2/${model_name}" \
