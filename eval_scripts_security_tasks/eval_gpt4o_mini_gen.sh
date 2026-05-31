23ea247c8b6f62a9f55efbf907ea59aac48c8071
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-31 16:22:07 -0600

    add script bash file for gpt model

---
+++ eval_scripts_security_tasks/eval_gpt4o_mini_gen.sh
@@ -0,0 +1,10 @@
+model_name="gpt-4o-mini"
+
+lm_eval --model openai-chat-completions \
+    --model_args "model=gpt-4o-mini" \
+    --tasks primevul_gen \
+    --batch_size auto \
+    --output_path "./output/primevul_gen/${model_name}" \
+    --limit 10 \
+    --log_samples \
+    --include_path ./custom_tasks_lm_eval
