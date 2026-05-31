14bec78ffdb3b64952d5fdc794e4eac07ef8b445
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-31 16:28:34 -0600

    add flag for gpt model bash file

--- eval_scripts_security_tasks/eval_gpt4o_mini_gen.sh
+++ eval_scripts_security_tasks/eval_gpt4o_mini_gen.sh
@@ -6,5 +6,6 @@ lm_eval --model openai-chat-completions \
     --batch_size auto \
     --output_path "./output/primevul_gen/${model_name}" \
     --limit 10 \
+    --apply_chat_template \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
