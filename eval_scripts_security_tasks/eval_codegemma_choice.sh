b51ab0d76af000aa015b58a2f6fdce68700b676e
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-06-01 16:21:28 -0600

    fix issue for codegemma-7b-it

--- eval_scripts_security_tasks/eval_codegemma_choice.sh
+++ eval_scripts_security_tasks/eval_codegemma_choice.sh
@@ -1,9 +1,9 @@
 model_path="google/codegemma-7b-it"
 model_name="CodeGemma-7B-IT"
 
-# primevul
+
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=6000,trust_remote_code=True,gpu_memory_utilization=0.50,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=6000" \
+    --model_args "pretrained=$model_path,max_model_len=5000,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.50,disable_custom_all_reduce=True,enforce_eager=True,dtype=bfloat16,max_num_batched_tokens=5000" \
     --tasks primevul_choice \
     --batch_size 1 \
     --output_path "./output/primevul_choice/${model_name}" \
