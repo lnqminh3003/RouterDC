125f97a0afb1a300342d20af155bdb47077b3d60
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-06-01 09:37:31 -0600

    fix OOM

--- eval_scripts_security_tasks/eval_gemma2_choice.sh
+++ eval_scripts_security_tasks/eval_gemma2_choice.sh
@@ -4,9 +4,9 @@ model_name="Gemma-2-9B-IT"
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.85,disable_custom_all_reduce=True" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.65,disable_custom_all_reduce=True,enforce_eager=True,max_num_batched_tokens=8192" \
     --tasks primevul_choice \
-    --batch_size 8 \
+    --batch_size 1 \
     --output_path "./output/primevul_choice/${model_name}" \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
--- eval_scripts_security_tasks/eval_virtue_choice.sh
+++ eval_scripts_security_tasks/eval_virtue_choice.sh
@@ -5,9 +5,9 @@ export TRITON_CACHE_DIR=/tmp/triton_cache_$$
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.55,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=2048" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.55,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=8192" \
     --tasks primevul_choice \
-    --batch_size 4 \
+    --batch_size 1 \
     --output_path "./output/primevul_choice/${model_name}" \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
