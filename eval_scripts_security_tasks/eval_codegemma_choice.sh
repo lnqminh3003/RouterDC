ebdb9995072cc89170021c4b9e0b38f1bf06b7f0
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-06-01 14:05:42 -0600

    fix OOM for gemma models

--- eval_scripts_security_tasks/eval_codegemma_choice.sh
+++ eval_scripts_security_tasks/eval_codegemma_choice.sh
@@ -3,7 +3,7 @@ model_name="CodeGemma-7B-IT"
 
 # primevul
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.65,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=8192" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.50,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=8192" \
     --tasks primevul_choice \
     --batch_size 1 \
     --output_path "./output/primevul_choice/${model_name}" \
--- eval_scripts_security_tasks/eval_gemma2_choice.sh
+++ eval_scripts_security_tasks/eval_gemma2_choice.sh
@@ -4,7 +4,7 @@ model_name="Gemma-2-9B-IT"
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.65,disable_custom_all_reduce=True,enforce_eager=True,max_num_batched_tokens=8192" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.50,disable_custom_all_reduce=True,enforce_eager=True,max_num_batched_tokens=8192" \
     --tasks primevul_choice \
     --batch_size 1 \
     --output_path "./output/primevul_choice/${model_name}" \
