ee08a74fc74e2c41dcd95727af99918be07151c8
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-31 22:37:20 -0600

    fix OOM issue

--- eval_scripts_security_tasks/eval_codegemma_choice.sh
+++ eval_scripts_security_tasks/eval_codegemma_choice.sh
@@ -3,9 +3,9 @@ model_name="CodeGemma-7B-IT"
 
 # primevul
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,gpu_memory_utilization=0.65,tensor_parallel_size=2,dtype=bfloat16,enforce_eager=True,max_num_batched_tokens=4096" \
     --tasks primevul_choice \
-    --batch_size auto \
+    --batch_size 4 \
     --output_path "./output/primevul_choice/${model_name}" \
     --log_samples \
     --include_path ./custom_tasks_lm_eval
