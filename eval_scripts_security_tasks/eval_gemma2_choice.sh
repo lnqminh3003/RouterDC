62fafc4b3f3d8918c6b9bd18933de142793772f7
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-05-31 22:27:24 -0600

    fix out of memory GPU issue

--- eval_scripts_security_tasks/eval_gemma2_choice.sh
+++ eval_scripts_security_tasks/eval_gemma2_choice.sh
@@ -2,10 +2,9 @@ model_path="google/gemma-2-9b-it"
 model_name="Gemma-2-9B-IT"
 
 export TRITON_CACHE_DIR=/tmp/triton_cache_$$
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 lm_eval --model vllm \
-    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.85" \
+    --model_args "pretrained=$model_path,max_model_len=8192,trust_remote_code=True,tensor_parallel_size=2,gpu_memory_utilization=0.85,disable_custom_all_reduce=True" \
     --tasks primevul_choice \
     --batch_size 8 \
     --output_path "./output/primevul_choice/${model_name}" \
