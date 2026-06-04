e7e23c815cdc46133d6778a71edfdf3374e90be9
Le Nguyen Quang Minh <lnqminh3003@gmail.com>
2026-06-04 15:40:14 -0600

    update path

--- train_scripts/router_train_primevul_gen_8models_shuffle2.sh
+++ train_scripts/router_train_primevul_gen_8models_shuffle2.sh
@@ -9,7 +9,7 @@ cluster_loss_weight=1
 seeds=(1)
 
 for seed in "${seeds[@]}"; do
-    PYTHONPATH=. ./venv/bin/python3 -u train_files/train_router_primevul_gen_8models_shuffle2.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --seed ${seed} --batch_size 64 --training_samples_per_dataset 8405 \
+    PYTHONPATH=. ./venv/bin/python3 -u train_files/train_router_primevul_gen_8models.py --training_steps ${training_steps} --top_k ${top_k} --last_k ${last_k} --learning_rate ${learning_rate} --eval_steps 50 --tempreture ${tempreture} --similarity_function ${similarity_function} --sample_loss_weight ${sample_loss_weight} --cluster_loss_weight ${cluster_loss_weight} --seed ${seed} --batch_size 64 --training_samples_per_dataset 8405 \
     --data_paths "./datasets/primevul/balance_8models/gen_cluster/train_shuffle2.json" \
     --test_data_paths "./datasets/primevul/balance_8models/gen/test.json" \
     --test_data_type "multi_attempt" \
